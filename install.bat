d0881e77d7041270c69f19231b072df2e891a4fb
SadiGadarkan <163028695+SadiGadarkan@users.noreply.github.com>
2024-03-13 04:37:11 +0800

    Add files via upload

---
+++ install.bat
@@ -0,0 +1,12 @@
+@echo off
+set "params=%*"
+cd /d "%~dp0" && ( if exist "%temp%\getadmin.vbs" del "%temp%\getadmin.vbs" ) && fsutil dirty query %systemdrive% 1>nul 2>nul || (  echo Set UAC = CreateObject^("Shell.Application"^) : UAC.ShellExecute "cmd.exe", "/k cd ""%~sdp0"" && %~s0 %params%", "", "runas", 1 >> "%temp%\getadmin.vbs" && "%temp%\getadmin.vbs" && exit /B )
+cls
+npm install console-title
+npm install prompt-sync
+npm install basic-msgbox
+npm install discord.js
+npm install readline
+npm install request
+npm install child_process
+start run.bat
